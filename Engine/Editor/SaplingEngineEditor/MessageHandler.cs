f1b2a92f4d477046e8ba884dc05e07986178fee8
guozhi <guozhi@xd.com>
2021-05-20 18:59:56 +0800

    添加鼠标移动事件枚举

--- Engine/Editor/SaplingEngineEditor/MessageHandler.cs
+++ Engine/Editor/SaplingEngineEditor/MessageHandler.cs
@@ -6,26 +6,27 @@ namespace SaplingEngineEditor
     public class MessageHandler : IMessageFilter
     {
         #region 消息ID
-        const int WM_DESTROY = 0x0002;
-        const int WM_SIZE = 0x0005;
-        const int WM_ACTIVATE = 0x0006;
-        const int WM_ENTERSIZEMOVE = 0x0231;
-        const int WM_EXITSIZEMOVE = 0x0232;
-        const int WM_MOUSEWHEEL = 0x020A;
-        const int WM_LBUTTONDOWN = 0x0201;
-        const int WM_LBUTTONUP = 0x0202;
-        const int WM_LBUTTONDBLCLK = 0x0203;
-        const int WM_RBUTTONDOWN = 0x0204;
-        const int WM_RBUTTONUP = 0x0205;
-        const int WM_RBUTTONDBLCLK = 0x0206;
-        const int WM_MBUTTONDOWN = 0x0207;
-        const int WM_MBUTTONUP = 0x0208;
-        const int WM_MBUTTONDBLCLK = 0x0209;
-        const int WM_KEYDOWN = 0x0100;
-        const int WM_KEYUP = 0x0101;
-        const int WM_SYSKEYDOWN = 0x0104;
-        const int WM_SYSKEYUP = 0x0105;
-        const int WM_CLOSE = 0x0010;
+        const int WM_DESTROY        = 0x0002;
+        const int WM_SIZE           = 0x0005;
+        const int WM_ACTIVATE       = 0x0006;
+        const int WM_ENTERSIZEMOVE  = 0x0231;
+        const int WM_EXITSIZEMOVE   = 0x0232;
+        const int WM_MOUSEWHEEL     = 0x020A;
+        const int WM_MOUSEMOVE      = 0x0200;
+        const int WM_LBUTTONDOWN    = 0x0201;
+        const int WM_LBUTTONUP      = 0x0202;
+        const int WM_LBUTTONDBLCLK  = 0x0203;
+        const int WM_RBUTTONDOWN    = 0x0204;
+        const int WM_RBUTTONUP      = 0x0205;
+        const int WM_RBUTTONDBLCLK  = 0x0206;
+        const int WM_MBUTTONDOWN    = 0x0207;
+        const int WM_MBUTTONUP      = 0x0208;
+        const int WM_MBUTTONDBLCLK  = 0x0209;
+        const int WM_KEYDOWN        = 0x0100;
+        const int WM_KEYUP          = 0x0101;
+        const int WM_SYSKEYDOWN     = 0x0104;
+        const int WM_SYSKEYUP       = 0x0105;
+        const int WM_CLOSE          = 0x0010;
         #endregion
 
         private readonly Panel directxPanel;
@@ -49,6 +50,7 @@ namespace SaplingEngineEditor
                     case WM_ENTERSIZEMOVE:
                     case WM_EXITSIZEMOVE:
                     case WM_MOUSEWHEEL:
+                    case WM_MOUSEMOVE:
                     case WM_LBUTTONDOWN:
                     case WM_LBUTTONUP:
                     case WM_LBUTTONDBLCLK:
